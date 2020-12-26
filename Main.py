29722eae86b0c8d72a80ca6d924fa16f452e6c55
FRotatori99 <rotatori.federico99@gmail.com>
2020-12-26 10:57:57 +0100

    Release 1

--- Main.py
+++ Main.py
@@ -4,7 +4,8 @@ import smtplib
 import pandas
 from string import Template
 
-MY_ADDRESS = 'admin@ketodiets360.com'
+MY_ADDRESS = 'admin@example.com'
+MY_PASSWORD = 'password'
 
 def load_template(templateName):
     with open('Emails/' + templateName + '.html', 'r', encoding='utf-8') as template_file:
@@ -15,28 +16,26 @@ def read_list_pandas(filename):
     df = pandas.read_csv(filename + '.csv', delimiter=',')
     return df
 
-# set up the SMTP server
 def connect_SMTP():
-    server = smtplib.SMTP(host='ketodiets360.com', port=587)#SSL - 465, NO SSL - 587
-    #server.ehlo()
+    server = smtplib.SMTP(host='yourDomain.com', port=587)
     server.starttls()
-    server.login('admin@ketodiets360.com', 'KetoDiets360$%&')
+    server.login(MY_ADDRESS, MY_PASSWORD)
     return server
 
 def send_msg(server, email, name, message_template):
     msg = MIMEMultipart()
     message = message_template.substitute(PERSON_NAME=name)
-    msg['From'] = 'KetoDiets360 <' + MY_ADDRESS + '>'
+    msg['From'] = 'Alias <' + MY_ADDRESS + '>'
     msg['To'] = email
-    -msg['Subject'] = 'Proof you can quickly transform your body'
+    msg['Subject'] = 'Test Email'
     msg.attach(MIMEText(message, 'html'))
     server.send_message(msg)
     del msg
 
-filename = 'list_test'
+filename = 'list'
 df = read_list_pandas(filename)
 server = connect_SMTP()
-template = load_template('C1_E1')
+template = load_template('mail1')
 for index, row in df.iterrows():
     print(f'Sending email to: {row["Email Address"]} - {row["First Name"]}')
     send_msg(server, row['Email Address'], row['First Name'], template)
